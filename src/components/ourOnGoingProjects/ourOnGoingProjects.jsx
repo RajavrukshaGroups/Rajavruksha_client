618d845885438273ebc0c56e5c9ed6ba39d80868
technical@rajavrukshagroup.in <monica.b.raju001@gmail.com>
2025-01-16 13:03:44 +0530

    fixed conflict

--- src/components/ourOnGoingProjects/ourOnGoingProjects.jsx
+++ src/components/ourOnGoingProjects/ourOnGoingProjects.jsx
@@ -8,19 +8,16 @@ import StarBorder from "../../utils/shinyButtons";
 import React from "react";
 import ShinyText from "../../utils/shinyButtons";
 import FadeContent from "../../utils/FadeContent";
-<<<<<<< HEAD
 import  Images from "../../components/assets/Sylan retreat_Banner1.webp"
 import { ContainerScroll } from "../ui/container-scroll-animation.jsx";
-
-=======
 import { MovingBorderDemo } from "../MovingBorder/MovingBorderDemo";
->>>>>>> f7d50c91dbd2a135a5e3b61460dcf0d3b2423ba3
+
+
 const OngoingProjects = () => {
   const disableContextMenu = (e) => {
     e.preventDefault();
   };
   return (
-<<<<<<< HEAD
     <div className="projects-container-new "> 
         {/* <FadeContent
                 blur={true}
@@ -66,69 +63,16 @@ const OngoingProjects = () => {
                 initialOpacity={0}
               >
           <h2 className="project-subtitle syl-retreat-title justify-center m-auto flex">Sylvan Retreat</h2>
-=======
-    <div className="projects-container-new">
-      <FadeContent
-        blur={true}
-        duration={600}
-        easing="ease-out"
-        initialOpacity={0}
-      >
-        <h1 className="projects-title">Our Ongoing Projects</h1>
-      </FadeContent>
-      <div className="project-card">
-        <div
-          className="project-image-container"
-          onContextMenu={disableContextMenu}
-        >
-          <img
-            // src="https://images.pexels.com/photos/259280/pexels-photo-259280.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            // src={bgImg}
-            src={SylRetreatHomePage}
-            alt="Green farmland under blue sky with white clouds"
-            className="project-image"
-          />
-        </div>
-        <div className="project-content">
-          <FadeContent
-            blur={true}
-            duration={900}
-            easing="ease-out"
-            initialOpacity={0}
-          >
-            <h2 className="project-subtitle syl-retreat-title">
-              Sylvan Retreat
-            </h2>
->>>>>>> f7d50c91dbd2a135a5e3b61460dcf0d3b2423ba3
           </FadeContent>
           <p className="project-description justify-center m-auto flex">
             Farmland is not just a dream; it's a sound financial investment with
             growing demand in the market.
           </p>
-<<<<<<< HEAD
     </div> 
         <div className="project-content" >
-           <Link to="/sylvan-retreat">
-              <button className="project-button">
-=======
-          {/* <Link to="/sylvan-retreat">
-                <button className="project-button">Read More</button>
-              </Link> */}
-
-          <StarBorder
-            as="button"
-            className="custom-class"
-            color="cyan"
-            speed="5s"
-          >
-            {/* <Link to="/sylvan-retreat">
-                <button className="project-button">Read More</button>
-              </Link> */}
-          </StarBorder>
-          <Link to="/sylvan-retreat">
+        <Link to="/sylvan-retreat">
             <button className="project-button">
               <MovingBorderDemo>
->>>>>>> f7d50c91dbd2a135a5e3b61460dcf0d3b2423ba3
                 <ShinyText
                   text="Read More"
                   disabled={false}
@@ -136,26 +80,9 @@ const OngoingProjects = () => {
                   className="project-button-wrapper"
                   gradient="linear-gradient(120deg, orange 40%, #ff8c00 50%, #ff8c00 60%)"
                 />
-<<<<<<< HEAD
-              </button>
-            </Link>
-=======
               </MovingBorderDemo>
             </button>
           </Link>
-          {/* <Link to="/sylvan-retreat">
-            <button className="project-button">
-                <ShinyText
-                  text="Read More"
-                  disabled={false}
-                  speed={3}
-                  className="project-button-wrapper"
-                  gradient="linear-gradient(120deg, orange 40%, #ff8c00 50%, #ff8c00 60%)"
-                />
-              </button>
-           
-            </Link> */}
->>>>>>> f7d50c91dbd2a135a5e3b61460dcf0d3b2423ba3
         </div>
       {/* </div> */}
     </div>
