f7d50c91dbd2a135a5e3b61460dcf0d3b2423ba3
RajavrukshaGroups <technical@rajavrukshagroup.in>
2025-01-16 11:31:53 +0530

    read more button

--- src/components/ourOnGoingProjects/ourOnGoingProjects.jsx
+++ src/components/ourOnGoingProjects/ourOnGoingProjects.jsx
@@ -8,21 +8,21 @@ import StarBorder from "../../utils/shinyButtons";
 import React from "react";
 import ShinyText from "../../utils/shinyButtons";
 import FadeContent from "../../utils/FadeContent";
+import { MovingBorderDemo } from "../MovingBorder/MovingBorderDemo";
 const OngoingProjects = () => {
   const disableContextMenu = (e) => {
     e.preventDefault();
   };
   return (
-    <div className="projects-container-new"> 
-        <FadeContent
-                blur={true}
-                duration={600}
-                easing="ease-out"
-                initialOpacity={0}
-              >
-
-      <h1 className="projects-title">Our Ongoing Projects</h1>
-              </FadeContent>
+    <div className="projects-container-new">
+      <FadeContent
+        blur={true}
+        duration={600}
+        easing="ease-out"
+        initialOpacity={0}
+      >
+        <h1 className="projects-title">Our Ongoing Projects</h1>
+      </FadeContent>
       <div className="project-card">
         <div
           className="project-image-container"
@@ -36,35 +36,38 @@ const OngoingProjects = () => {
             className="project-image"
           />
         </div>
-        <div className="project-content" >
-        <FadeContent
-                blur={true}
-                duration={900}
-                easing="ease-out"
-                initialOpacity={0}
-              >
-          <h2 className="project-subtitle syl-retreat-title">Sylvan Retreat</h2>
+        <div className="project-content">
+          <FadeContent
+            blur={true}
+            duration={900}
+            easing="ease-out"
+            initialOpacity={0}
+          >
+            <h2 className="project-subtitle syl-retreat-title">
+              Sylvan Retreat
+            </h2>
           </FadeContent>
           <p className="project-description">
             Farmland is not just a dream; it's a sound financial investment with
             growing demand in the market.
           </p>
-            {/* <Link to="/sylvan-retreat">
+          {/* <Link to="/sylvan-retreat">
                 <button className="project-button">Read More</button>
               </Link> */}
 
-              <StarBorder
-                as="button"
-                className="custom-class"
-                color="cyan"
-                speed="5s"
-              >
-              {/* <Link to="/sylvan-retreat">
+          <StarBorder
+            as="button"
+            className="custom-class"
+            color="cyan"
+            speed="5s"
+          >
+            {/* <Link to="/sylvan-retreat">
                 <button className="project-button">Read More</button>
               </Link> */}
           </StarBorder>
-            <Link to="/sylvan-retreat">
-              <button className="project-button">
+          <Link to="/sylvan-retreat">
+            <button className="project-button">
+              <MovingBorderDemo>
                 <ShinyText
                   text="Read More"
                   disabled={false}
@@ -72,9 +75,10 @@ const OngoingProjects = () => {
                   className="project-button-wrapper"
                   gradient="linear-gradient(120deg, orange 40%, #ff8c00 50%, #ff8c00 60%)"
                 />
-              </button>
-            </Link>
-            {/* <Link to="/sylvan-retreat">
+              </MovingBorderDemo>
+            </button>
+          </Link>
+          {/* <Link to="/sylvan-retreat">
             <button className="project-button">
                 <ShinyText
                   text="Read More"
