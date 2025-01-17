8e542ee3172f4f6ec138d98be8d1677b993ca039
RajavrukshaGroups <technical@rajavrukshagroup.in>
2025-01-17 12:13:49 +0530

    few more changes

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -155,7 +155,7 @@ const About = ({ text, readMore }) => {
             </div>
           </div>
           <div className="row">
-            <div className="col-lg-6 col-md-12">
+            <div className="col-lg-6 col-md-12 vision-new">
               <div
                 className="about-left"
                 data-aos="fade-right"
@@ -213,7 +213,7 @@ const About = ({ text, readMore }) => {
                 )}
               </div>
             </div>
-            <div className="col-lg-6 col-md-12">
+            <div className="col-lg-6 col-md-12 mission-new">
               <div
                 className="about-left"
                 data-aos="fade-left"
