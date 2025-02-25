a1f7c7f90e13b4f0bf1b7e08ff5221b5fd28c1f0
RajavrukshaGroups <technical@rajavrukshagroup.in>
2025-02-25 10:00:24 +0530

    runnin banner-eco

--- src/components/Projects/SylvanNewRunner.jsx
+++ src/components/Projects/SylvanNewRunner.jsx
@@ -1,10 +1,32 @@
+import { text } from "@fortawesome/fontawesome-svg-core";
 import "./SylvanNewRunner.css";
+import {
+  IconPlaneDeparture,
+  IconBusStop,
+  IconReceiptRupee,
+  IconSchool,
+  IconBuildingFactory2,
+  IconBuildingHospital,
+  IconCar,
+  icons,
+  IconRoad,
+} from "@tabler/icons-react";
+
 const SylvanNewRunner = () => {
   const points = [
-    "Located Near State Highway-09",
-    "25-Minute Drive From KIAL",
-    "6 KM From Shri Ghati Subrahmanya Temple",
-    "KHIR City is only 15 Minutes Away",
+    { text: "Located Near State Highway-09", icon: <IconRoad size={18} /> },
+    {
+      text: "25-Minute Drive From KIAL",
+      icon: <IconPlaneDeparture size={18} />,
+    },
+    {
+      text: "6 KM From Shri Ghati Subrahmanya Temple",
+      icon: <IconCar size={18} />,
+    },
+    {
+      text: "KHIR City Is Only 15 Minutes Away",
+      icon: <IconBuildingFactory2 size={18} />,
+    },
   ];
   return (
     <div className="sylvan-runner-wrapper">
@@ -12,14 +34,16 @@ const SylvanNewRunner = () => {
         <div className="sylvan-runner-content">
           {points.map((point, index) => (
             <span key={index} className="sylvan-runner-point">
-              {point} •
+              <div className="sylvan-runner-icon">{point.icon}</div>
+              <div className="sylvan-runner-text">{point.text}</div>
             </span>
           ))}
         </div>
         <div className="sylvan-runner-content">
           {points.map((point, index) => (
             <span key={index} className="sylvan-runner-point">
-              {point} •
+              <div className="sylvan-runner-icon">{point.icon}</div>
+              <div className="sylvan-runner-text">{point.text}</div>
             </span>
           ))}
         </div>
