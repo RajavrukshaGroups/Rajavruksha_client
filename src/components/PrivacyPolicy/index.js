65fdbd8e8982ede2f47301e8fea05600808a207e
RajavrukshaGroups <technical@rajavrukshagroup.in>
2025-03-08 10:08:09 +0530

    new changes to privacy policy

--- src/components/PrivacyPolicy/index.js
+++ src/components/PrivacyPolicy/index.js
@@ -19,7 +19,11 @@ const PrivacyPolicy = () => {
       <div className="introduction">
         <p className="para-heading">1. Introduction </p>
         <p className="para-text">
-        We at Rajavruksha Realtors Pvt. Ltd. are committed to protecting your privacy. This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you visit our website. Please read this policy carefully. If you disagree with the terms of this privacy policy, please do not access the site.
+          We at Rajavruksha Realtors Pvt. Ltd. are committed to protecting your
+          privacy. This Privacy Policy explains how we collect, use, disclose,
+          and safeguard your information when you visit our website. Please read
+          this policy carefully. If you disagree with the terms of this privacy
+          policy, please do not access the site.
         </p>
         {/* <p className="para-text">
           <span>Personal Information:</span> Name, address, contact details,
@@ -32,67 +36,87 @@ const PrivacyPolicy = () => {
         </p> */}
       </div>
       <div className="introduction">
-        <p className="para-heading">2.  Information</p>
+        <p className="para-heading">2. Information</p>
         <ul className="list-methods">
           <p className="para-text">
-          We Collect Personal Data We may collect personally identifiable information "Personal Data" about you when you voluntarily provide this information to us. This may include: - Name - Email address - Telephone number - Mailing address - Location - Any other information you provide to us directly
+            We Collect Personal Data We may collect personally identifiable
+            information "Personal Data" about you when you voluntarily provide
+            this information to us. This may include: - Name - Email address -
+            Telephone number - Mailing address - Location - Any other
+            information you provide to us directly
           </p>
-          
         </ul>
       </div>
       <div className="introduction">
         <p className="para-heading">3. How We Use Your Information</p>
         <p className="para-text">
-        We use the information we collect in the following ways:
-          
+          We use the information we collect in the following ways:
         </p>
         <ul className="list-methods">
-        
           <li className="list-text privacy-points">
-          To provide, operate, and maintain our Site.
+            To provide, operate, and maintain our Site.
           </li>
           <li className="list-text privacy-points">
-          To improve, personalize, and expand our Site
+            To improve, personalize, and expand our Site
           </li>
           <li className="list-text privacy-points">
             To improve the functionality and performance of our website.
           </li>
           <li className="list-text privacy-points">
-          To understand and analyse how you use our Site 
+            To understand and analyse how you use our Site
           </li>
           <li className="list-text privacy-points">
-          To develop new products, services, features 
+            To develop new products, services, features
           </li>
         </ul>
-        
-        
       </div>
       <div className="introduction">
         <p className="para-heading">4. Sharing Your Information</p>
         <p className="para-text">
-        We do not sell, trade, or rent your personal identification information to others. We may share generic aggregated demographic information not linked to any personal identification information regarding visitors and users with our business partners, trusted affiliates, and advertisers.
+          We do not sell, trade, or rent your personal identification
+          information to others. We may share generic aggregated demographic
+          information not linked to any personal identification information
+          regarding visitors and users with our business partners, trusted
+          affiliates, and advertisers.
         </p>
       </div>
       <div className="introduction">
         <p className="para-heading"> 5. Protecting Your Information </p>
         <p className="para-text">
-        We adopt appropriate data collection, storage, and processing practices and security measures to protect against unauthorized access, alteration, disclosure, or destruction of your personal information, username, password, transaction information, and data stored on our Site
+          We adopt appropriate data collection, storage, and processing
+          practices and security measures to protect against unauthorized
+          access, alteration, disclosure, or destruction of your personal
+          information, username, password, transaction information, and data
+          stored on our Site
         </p>
       </div>
       <div className="introduction">
-        <p className="para-heading"> 6. Changes to This Privacy Policy </p>
-        <p className="para-text">Rajavruksha Realtors Pvt. Ltd. can update this privacy policy at any time. We encourage users to frequently check this page for any changes to stay informed about how we are helping to protect the personal information we collect. By using this Site, you signify your acceptance of this policy. If you do not agree to our above policies you could restrain from the use of our website. Your continued use of the Site following the posting of changes to this policy will be deemed your acceptance of those changes. With the terms of this privacy policy, please do not access the site.</p>
-        
+        <p className="para-heading"> 6. Electronic Communications </p>
+        <p className="para-text">
+          {/* Rajavruksha Realtors Pvt. Ltd. can update this privacy policy at any
+          time. We encourage users to frequently check this page for any changes
+          to stay informed about how we are helping to protect the personal
+          information we collect. By using this Site, you signify your
+          acceptance of this policy. If you do not agree to our above policies
+          you could restrain from the use of our website. Your continued use of
+          the Site following the posting of changes to this policy will be
+          deemed your acceptance of those changes. With the terms of this
+          privacy policy, please do not access the site. */}
+          When you voluntarily send us electronic mail / fillup the form, we
+          will keep a record of this information so that we can respond to you.
+          We only collect information from you when you register on our site or
+          fill out a form. Also, when filling out a form on our site, you may be
+          asked to enter your: name, e-mail address or phone number. You may,
+          however, visit our site anonymously. In case you have submitted your
+          personal information and contact details, we reserve the rights to
+          Call, SMS, Email or WhatsApp about our products and offers, even if
+          your number has DND activated on it.
+        </p>
       </div>
-      
     </div>
   );
 };
 export default PrivacyPolicy;
-
-
-
-
 
 // old privacy package
 // export default PrivacyPolicy;
